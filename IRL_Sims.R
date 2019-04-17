152afd1004ba3d596a7704daf730c9c793638a03
KevinGunn <kpgunn@ncsu.edu>
2019-04-17 16:11:18 -0400

    Practice

---
+++ IRL_Sims.R
@@ -0,0 +1,9 @@
+###################################################
+## Kevin Gunn                                    ##
+## 4/17/2019                                     ##
+## Project 2 - Constrained IRL                   ##
+##                                               ##
+###################################################
+
+# Git: https://github.com/KevinGunn/Project2_Sims.git
+
