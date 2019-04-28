b5ee9994781e47cb6b2adcd9641c78c4957d72d4
KevinGunn <kpgunn@ncsu.edu>
2019-04-28 18:42:03 -0400

    New R Program

--- IRL_SIMS_Lambda_known.R
+++ IRL_SIMS_Lambda_known.R
@@ -142,7 +142,7 @@ for(i in 1:nrow(grid)){
   
   eta_initial <- rep(0,5)
   
-
+  
   eta_opt <- nmk(par = eta_initial, 
                  eta_func,
                  control=list(maximize=TRUE)
@@ -186,75 +186,64 @@ plot_3d
 
 # Based on scatterplot let M=3.7, lambda=0.2, and V = 0.1164.
 EY_EZ
-M_opt = gv_df[167,1]; lambda_opt = gv_df[167,2]; eta_opt <- eta_opt_mat[167,]
-#M_opt = gv_df[3,1]; lambda_opt = gv_df[3,2]; eta_opt <- eta_opt_mat[3,]
+M_opt = gv_df[51,1]; lambda_opt = gv_df[51,2]; eta_opt <- eta_opt_mat[51,]
 
 #################################################################################
 
-#Generation of Data assuming clinician makes optimal decision.
+# number of simulations is set for 500.
+num_sims <- 500
+lambda_est <- lambda_opt
 
-# Generate Covariates Data 
-X <- mvrnorm(n=n, mu=rep(0,p), diag(p) )
+#Store output in following matrices/vectors.
+eta_mat_lambda <- matrix(0, ncol=p, nrow = num_sims)
 
-# 100% Correct decisions
-opt_model <- as.vector(eta_opt %*% t(X))
+M_est_vec <- rep(0, num_sims )
+V_est_vec <- rep(0, num_sims )
+V_clin_vec <- rep(0, num_sims )
 
-clin_model <- opt_model
-
-################
-# Add error to optimal model so clinician is slightly imperfect.
-#clin_model <- opt_model + rnorm(n,0,1.5)
-
-# Clinician is approximately 91% correct.
-#sum(trt_rule(opt_model) == trt_rule(clin_model))  
-###############
-
-# Treatment
-A <- trt_rule(clin_model)
-
-# Main Outcome of Interest
-Y <- as.vector( theta_0Y %*% t(X) ) + A * as.vector( theta_1Y %*% t(X) ) + rnorm(n,sd=0.5)
-
-# Risk Outcome
-Z <- as.vector( theta_0Z %*% t(X) ) + A * as.vector( theta_1Z %*% t(X) ) + rnorm(n,sd=0.5)
+VY_est_vec <- rep(0, num_sims )
+VZ_est_vec <- rep(0, num_sims )
 
 
-###################################################################################
+VY_clin_vec <- rep(0, num_sims )
+VZ_clin_vec <- rep(0, num_sims )
 
+PCD_vec <- rep(0, num_sims )
 
+for(sim in 1:num_sims){
 
-# Q-model to find theta values
-
-QY <- lm(Y ~ X + A:X)
-QZ <- lm(Z ~ X + A:X)
-
-params_est <- list(QY$coefficients[2:(p+1)], QY$coefficients[-(1:(p+1))], 
-                   QZ$coefficients[2:(p+1)], QZ$coefficients[-(1:(p+1))],
-                   QY$coefficients[1],QZ$coefficients[1])
-
-
-# Start algorithm.
-# Create empty matrices to store values in.
-lin_rules_mat <- matrix(0,ncol=5,nrow=sims)
-
-
-#Step 1 - Grid Search
-#lambda_est <- c(1,1.7,2.0)
-lambda_est <- c(0.15,0.25,0.05)
-
-
-eta_mat <- matrix(0,ncol=p,nrow=50)
-k_eta_mat_lambda <- matrix(0, ncol=p, nrow = length(lambda_est))
-
-M_est <- rep(0, length(lambda_est) )
-V_est <- rep(0, length(lambda_est) )
-
-
-for (i in 1:length(lambda_est)) {
+  # Generate Covariates Data 
+  X <- mvrnorm(n=n, mu=rep(0,p), diag(p) )
+  
+  # 100% Correct decisions
+  opt_model <- as.vector(eta_opt %*% t(X))
+  
+  clin_model <- opt_model
+  
+  # Treatment
+  A <- trt_rule(clin_model)
+  
+  # Main Outcome of Interest
+  Y <- as.vector( theta_0Y %*% t(X) ) + A * as.vector( theta_1Y %*% t(X) ) + rnorm(n,sd=0.5)
+  
+  # Risk Outcome
+  Z <- as.vector( theta_0Z %*% t(X) ) + A * as.vector( theta_1Z %*% t(X) ) + rnorm(n,sd=0.5)
+  
+  
+  ###################################################################################
+  # Q-model to find theta values
+  
+  QY <- lm(Y ~ X + A:X)
+  QZ <- lm(Z ~ X + A:X)
+  
+  params_est <- list(QY$coefficients[2:(p+1)], QY$coefficients[-(1:(p+1))], 
+                     QZ$coefficients[2:(p+1)], QZ$coefficients[-(1:(p+1))],
+                     QY$coefficients[1],QZ$coefficients[1])
+  
   
   # Clinicians Value function
   V_Y_clin <-  mean(Y)
-  V_Z_clin_tol <- mean(Z) - lambda_est[i]  #EY_EZ[167,2] - lambda_est[i]
+  V_Z_clin_tol <- mean(Z) - lambda_est  #EY_EZ[167,2] - lambda_est[i]
   
   mu_clin <- c( V_Y_clin, hinge(V_Z_clin_tol) )
   
@@ -264,11 +253,11 @@ for (i in 1:length(lambda_est)) {
   rule1 <- trt_rule(lin_mod1)
   
   VY_learner <- VY_est(rule1)
-  VZ_learner <- VZ_tol_est(rule1, lambda_est[i])
+  VZ_learner <- VZ_tol_est(rule1, lambda_est)
   
   mu_learner <- c(VY_learner, hinge(VZ_learner) )
   
-  # Begin AL-IRL
+  # Begin AL-IRL Algorithm.
   k = 1;q=1;eps = 0.0001
   
   eta_opt_k <- lin_rule_coeff
@@ -301,10 +290,10 @@ for (i in 1:length(lambda_est)) {
     
     bv <- c(1,rep(0,k))
     sol <- solve.QP(Dm,dv,t(Am),bv,meq=1)
-
+    
     Mk = sol$solution[2]/sol$solution[1]
     qk = sol$solution[3]
-     
+    
     
     # Plot of Value functions.
     plot(L_mu_mat[,-1],col="blue", pch=19, main="Value Function Estimation",cex=1.2)
@@ -312,10 +301,10 @@ for (i in 1:length(lambda_est)) {
     # Hyperplane
     #abline( b=1/Mk , col="green", lty=4)
     
-  
+    
     # Obtain estimate of eta^opt
     eta_func <- function(eta){ value_eta(eta, X=X, params_list = params_est,
-                                         M=Mk, lambda = lambda_est[i]) }
+                                         M=Mk, lambda = lambda_est) }
     
     eta_opt_k <- nmk(par = rep(0,5) , eta_func, control=list(maximize=TRUE))$par
     
@@ -324,7 +313,7 @@ for (i in 1:length(lambda_est)) {
     rulek <- trt_rule(lin_modk)
     
     VY_learner <- VY_est(rulek)
-    VZ_learner <- VZ_tol_est(rulek, lambda_est[i])
+    VZ_learner <- VZ_tol_est(rulek, lambda_est)
     
     # Update data
     mu_learner <- c(VY_learner, hinge(VZ_learner) )
@@ -339,11 +328,74 @@ for (i in 1:length(lambda_est)) {
     
   }
   
-  M_est[i] <- Mk
-  k_eta_mat_lambda[i,] <-  eta_opt_k
-  V_est[i] <-  mu_learner[1] + Mk*mu_learner[2]
+  M_est_vec[sim] <- Mk
+  eta_mat_lambda[sim,] <-  eta_opt_k
+  V_est_vec[sim] <-  mu_learner[1] + Mk*mu_learner[2]
+
+  V_clin_vec[sim] <- mu_clin[1] + M_opt*mu_clin[2]
   
+  VY_est_vec[sim] <- mu_learner[1]
+  VZ_est_vec[sim] <- mu_learner[2]
+  
+  VY_clin_vec[sim] <- mu_clin[1]
+  VZ_clin_vec[sim] <- mu_clin[2]
+  
+  PCD_vec[sim] <- sum(rulek==A)/n
 }
+
+eta_bias <- apply(eta_mat_lambda,2,mean) - eta_opt
+
+eta_sd <- apply(eta_mat_lambda,2,sd)
+
+M_est_mean <- mean(M_est_vec)
+M_est_sd <- sd(M_est_vec)
+
+V_est_mean <- mean(V_est_vec)
+V_est_sd <- sd(V_est_vec)
+
+V_clin_mean <- mean(V_clin_vec)
+V_clin_sd <- sd(V_clin_vec)
+
+VY_est_mean <- mean(VY_est_vec)
+VY_est_sd <- sd(VY_est_vec)
+
+VY_clin_mean <- mean(VY_clin_vec)
+VY_clin_sd <- sd(VY_clin_vec)
+
+VZ_est_mean <- mean(VZ_est_vec)
+VZ_est_sd <- sd(VZ_est_vec)
+
+
+VZ_clin_mean <- mean(VZ_clin_vec)
+VZ_clin_sd <- sd(VZ_clin_vec)
+
+pcd_mean <- mean(PCD_vec)
+pcd_sd <- sd(PCD_vec)
+
+IRL_list <- list(
+  
+  eta_bias = eta_bias,
+  eta_sd = eta_sd,
+  M_est_mean = M_est_mean,
+  M_est_sd = M_est_sd,
+  V_est_mean = V_est_mean,
+  V_est_sd = V_est_sd,
+  V_clin_mean = V_clin_mean,
+  V_clin_sd = V_clin_sd,
+  VY_est_mean = VY_est_mean,
+  VY_est_sd = VY_est_sd,
+  VY_clin_mean = VY_clin_mean,
+  VY_clin_sd = VY_clin_sd,
+  VZ_est_mean = VZ_est_mean,
+  VZ_est_sd = VZ_est_sd,
+  VZ_clin_mean = VZ_clin_mean,
+  VZ_clin_sd = VZ_clin_sd,
+  pcd_mean = pcd_mean,
+  pcd_sd = pcd_sd
+  
+)
+
+capture.output(IRL_list, file = "IRL_known_lambda.txt")
 
 
 
