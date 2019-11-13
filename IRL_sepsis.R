ec0d78fc4ddb42a743834d1249a41feb9b386902
Kevin Gunn <kpgunn@ncsu.edu>
2019-11-13 16:57:05 -0500

    Add files via upload

--- IRL_sepsis.R
+++ IRL_sepsis.R
@@ -150,29 +150,31 @@ mean( sepdat_iv_dose_b1$reward )
 
 # Value Functions.
 
+# Augmented IPWE
 AIPWE_b <- function( Y, A, X, prop_score, eta){
   
   g_x_eta <- ifelse(eta %*% t(X) > 0, 1, 0)
   
   C_eta <- A*g_x_eta + (1-A)*( 1 - g_x_eta )
   
-  prop_c <- prop_score*g_x_eta + (1-prop_score)*( 1 - g_x_eta )
+  prop_c <- prop_score*A + (1-prop_score)*( 1 - A )
   
   #dat <- data.frame(Y,A,X)
   #print(dat$X)
-  mu <- glm(Y ~ X + A*X, family = binomial(link = "logit"))
+  mu <- glm(Y ~ X[,-1] + A*X[,-1], family = binomial(link = "logit"))
   
   A <- rep(1, length(Y))
-  dat.m1 <- data.frame(X, A*X)
+  dat.m1 <- data.frame(X[,-1], A*X[,-1])
   A <- rep(0, length(Y))
-  dat.m0 <- data.frame(X, A*X)
+  dat.m0 <- data.frame(X[,-1], A*X[,-1])
   
   mu_1 <- predict(mu, dat.m1)
   mu_0 <- predict(mu, dat.m0)
   
   m_x <- mu_1*g_x_eta + mu_0*( 1 - g_x_eta )
   
-  AIPWE <- mean( ( C_eta * Y ) / prop_c - ( ( C_eta - prop_c) / prop_c )*m_x )
+  AIPWE <- mean( ( as.vector( C_eta * Y ) / as.vector(prop_c) ) - 
+                   ( as.vector( C_eta - as.vector(prop_c)) / as.vector(prop_c) )*as.vector(m_x) )
   
   return(AIPWE)
 }
@@ -183,34 +185,60 @@ AIPWE_c <- function( Z, A, X, prop_score, eta){
   
   C_eta <- A*g_x_eta + (1-A)*( 1 - g_x_eta )
   
-  prop_c <- prop_score*g_x_eta + (1-prop_score)*( 1 - g_x_eta )
+  prop_c <- prop_score*A + (1-prop_score)*( 1 - A )
   
-  dat <- data.frame(Z,A,X)
-  mu <- lm(Y ~ X + A*X, data=dat)
-  
+  mu <- lm( Z ~ X[,-1] + A*X[,-1] )
 
   A <- rep(1, length(Z))
-  dat.m1 <- data.frame(X, A*X)
+  dat.m1 <- data.frame(X[,-1], A*X[,-1])
   A <- rep(0, length(Z))
-  dat.m0 <- data.frame(X, A*X)
+  dat.m0 <- data.frame(X[,-1], A*X[,-1])
   
   mu_1 <- predict(mu, dat.m1)
   mu_0 <- predict(mu, dat.m0)
   
   m_x <- mu_1*g_x_eta + mu_0*( 1 - g_x_eta )
   
-  AIPWE <- mean( ( C_eta * Y ) / prop_c - ( ( C_eta - prop_c) / prop_c )*m_x )
+  AIPWE <- mean( ( as.vector( C_eta * Z ) / as.vector(prop_c) ) - 
+                   ( as.vector( C_eta - as.vector(prop_c)) / as.vector(prop_c) )*as.vector(m_x) )
   
   return(AIPWE)
 }
 
+# IPWE
+IPWE_b <- function( Y, A, X, prop_score, eta){
+  
+  g_x_eta <- ifelse(eta %*% t(X) > 0, 1, 0)
+  
+  C_eta <- A*g_x_eta + (1-A)*( 1 - g_x_eta )
+  
+  prop_c <- prop_score*A + (1-prop_score)*( 1 - A )
+  
+  IPWE <- mean( as.vector( C_eta * Y ) / as.vector(prop_c ) )
+  
+  return(IPWE)
+}
 
+IPWE_c <- function( Z, A, X, prop_score, eta){
+  
+  g_x_eta <- ifelse(eta %*% t(X) > 0, 1, 0)
+  
+  C_eta <- A*g_x_eta + (1-A)*( 1 - g_x_eta )
+  
+  prop_c <- prop_score*A + (1-prop_score)*( 1 - A )
+  
+  IPWE <- mean( as.vector( C_eta * Z ) / as.vector(prop_c) )
+  
+  return(IPWE)
+}
+
+# NEED TO CHANGE IPWE IN OPT.ETA function.
 opt.eta <- function(eta, Y, Z, A, X, prop_score, M, lambda){
   
-  VY <- AIPWE_b(Y, A, X, propscore, eta)
-  VZ <- AIPWE_c(Z, A, X, propscore, eta)
+  VY <- IPWE_b(Y, A, X, prop_score, eta)
+  VZ <- IPWE_c(Z, A, X, prop_score, eta)
   
-  return( VY - M*(VZ-lambda)*as.numeric(VZ>lambda) )
+  return( - (VZ - M*(VY-lambda)*as.numeric(VY>lambda)) )
 }
 
 # Condition handling code to avoid bad starting values in nmk.
@@ -250,11 +278,14 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
   rule1 <- trt_rule(lin_mod1)
   
   #rule1
-  print(mean(rule1 == A))
+  print( c("eta 0:", eta0) )
+  print( c("rule 0", mean(rule1 == A)) )
   
   # Initial estimated rule
-  VY_learner <- AIPWE_b(Y,A,X,prop_score,eta0)
-  VZ_learner <- AIPWE_c(Z,A,X,prop_score,eta0)
+  #VY_learner <- AIPWE_b(Y,A,X,prop_score,eta0)
+  #VZ_learner <- AIPWE_c(Z,A,X,prop_score,eta0)
+  VY_learner <- IPWE_b(Y,A,X,prop_score,eta0)
+  VZ_learner <- IPWE_c(Z,A,X,prop_score,eta0)
   
   mu_learner <- c( VY_learner, hinge( VZ_learner - lambda ) )
   
@@ -307,7 +338,7 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
     
     # Plot of Value functions.
     plot(L_mu_mat[-1,-1],col="blue", pch=19, main="Value Function Estimation",
-         xlab="Risk 1", ylab = "Risk 2", cex=1.2) #, ylim = c(0.29,0.39), xlim = c(0.22,0.38))
+         xlab="Risk 1", ylab = "Risk 2", cex=1.2, ylim = c(0,1), xlim = c(0,1))
     points(x= L_mu_mat[1,2],y= L_mu_mat[1,3],bg="red",pch=22,cex=1.5) 
     text(L_mu_mat[-1,-1], labels = seq(from=1, to = k) , pos = 4 )
     text(x = L_mu_mat[1,2], y= L_mu_mat[1,3], labels = "Clinicians" , pos = 1 )
@@ -332,12 +363,13 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
     #etarec<-matrix(nrow=length(alps),ncol=npar)
     #fvals<-matrix(nrow=length(alps),ncol=3)
     
-    fval<-100
+    fval<-Inf
     for(j in 1:nrow(eta_start)){
-      fit<-try(optim(par=eta_start[j,],fn=opt.eta,Y=Y,Z=Z,A=A,X=X,lambda=lambda,M=Mk),silent=TRUE)
+      fit<-try(optim(par=eta_start[j,],fn=opt.eta,Y=Y,Z=Z,A=A,X=X,prop_score=prop_score,
+                     lambda=lambda,M=Mk),silent=TRUE)
       if(!is.character(fit)){
         if(fit$value<fval){
-          eta<-fit$par/sqrt(sum(fit$par^2))
+          eta<- fit$par/sqrt(sum(fit$par^2))
           #etarec[l,]<-eta
           fval<-fit$value
           cat(paste("j=",j,"\n"))
@@ -345,7 +377,6 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
         }
       }
     }
-    
     #fit<-try(genoud(opt.eta,nvars=npar,max=FALSE,starting.values=fit$par,max.generations=30,print.level=0,mydata=data,tt0=tt0.in,alp=lambda,M=Mk),silent=TRUE)
     #if(!is.character(fit)){
     #  if(fit$value<fval){
@@ -357,9 +388,9 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
     #  }
     #}
     
-    
+    #print(eta)
     eta_k <- eta
-    #print(eta_k)
+    print(c("eta k:", eta_k) )
     # Store eta values
     eta_store[k,] <- eta_k
     
@@ -371,8 +402,10 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
     print( c("rule_k",mean(rulek == A)) )
     
     # k estimated rule and value functions.
-    VY_learner <- AIPWE_b(Y,A,X,prop_score,eta_k)
-    VZ_learner <- AIPWE_c(Z,A,X,prop_score,eta_k)
+    #VY_learner <- AIPWE_b(Y,A,X,prop_score,eta_k)
+    #VZ_learner <- AIPWE_c(Z,A,X,prop_score,eta_k)
+    VY_learner <- IPWE_b(Y,A,X,prop_score,eta_k)
+    VZ_learner <- IPWE_c(Z,A,X,prop_score,eta_k)
     
     # Update Value functions.
     mu_learner <- c(VY_learner, hinge(VZ_learner - lambda) )
@@ -393,7 +426,7 @@ QP_IRL <- function(Y, Z, A, X, prop_score, k.num, eps, lambda, eta0){
                    M_est = Mk,
                    eps_k = qk,
                    Value_mat = L_mu_mat,
-                   trt_match = sum(rulek == data$A) / dim(data)[1],
+                   trt_match = sum(rulek == A) / length(A),
                    decision_rule = rulek
   )
   
@@ -412,27 +445,67 @@ reward[which(sepdat_iv_dose_b1$reward== 15)] <- 0
 reward[which(sepdat_iv_dose_b1$reward== -15)] <- 0
 
 # Recode died in hospital variable.
-recode_dh <- sepdat_iv_dose_b1$died_in_hosp
-recode_dh[which(sepdat_iv_dose_b1$died_in_hosp == 1)] <- 0
-recode_dh[which(sepdat_iv_dose_b1$died_in_hosp == 0)] <- 1
+#recode_dh <- sepdat_iv_dose_b1$died_in_hosp
+#recode_dh[which(sepdat_iv_dose_b1$died_in_hosp == 1)] <- 0
+#recode_dh[which(sepdat_iv_dose_b1$died_in_hosp == 0)] <- 1
+
+# Covariates for glmnet
+xvars_noscale <- cbind(sepdat_iv_dose_b1$gender, sepdat_iv_dose_b1$age, sepdat_iv_dose_b1$elixhauser, 
+                       sepdat_iv_dose_b1$Weight_kg, sepdat_iv_dose_b1$GCS, sepdat_iv_dose_b1$HR, sepdat_iv_dose_b1$SysBP,
+                       sepdat_iv_dose_b1$MeanBP, sepdat_iv_dose_b1$DiaBP, sepdat_iv_dose_b1$RR,                                         
+                       sepdat_iv_dose_b1$SpO2, sepdat_iv_dose_b1$Temp_C, sepdat_iv_dose_b1$FiO2_1, 
+                       sepdat_iv_dose_b1$Potassium, sepdat_iv_dose_b1$Sodium, sepdat_iv_dose_b1$Chloride,
+                       sepdat_iv_dose_b1$Glucose, sepdat_iv_dose_b1$BUN, sepdat_iv_dose_b1$Creatinine,                                
+                       sepdat_iv_dose_b1$Magnesium, sepdat_iv_dose_b1$Calcium, sepdat_iv_dose_b1$Ionised_Ca,
+                       sepdat_iv_dose_b1$CO2_mEqL, sepdat_iv_dose_b1$SGOT, sepdat_iv_dose_b1$SGPT, sepdat_iv_dose_b1$Total_bili, sepdat_iv_dose_b1$Albumin, 
+                       sepdat_iv_dose_b1$Hb, sepdat_iv_dose_b1$WBC_count, sepdat_iv_dose_b1$Platelets_count,
+                       sepdat_iv_dose_b1$PTT, sepdat_iv_dose_b1$PT, sepdat_iv_dose_b1$INR, sepdat_iv_dose_b1$Arterial_pH,
+                       sepdat_iv_dose_b1$paO2, sepdat_iv_dose_b1$paCO2, sepdat_iv_dose_b1$Arterial_BE, sepdat_iv_dose_b1$Arterial_lactate,
+                       sepdat_iv_dose_b1$HCO3, sepdat_iv_dose_b1$mechvent, sepdat_iv_dose_b1$Shock_Index,
+                       sepdat_iv_dose_b1$PaO2_FiO2, sepdat_iv_dose_b1$SOFA, sepdat_iv_dose_b1$SIRS)
+
+xvars <- cbind(xvars_noscale[, c(1,40)], log(xvars_noscale[, 43 ]+1), log( xvars_noscale[, 44] + 1),
+               apply( xvars_noscale[, -c(1,40,43,44)], 2 , scale ) )
+
+colnames(xvars) <- c( "gender", "mechvent", "SOFA", "SIRS", "age", "elixhauser", "Weight_kg", "GCS",
+                      "HR", "SysBP", "MeanBP", "DiaBP", "RR", "SpO2", "Temp_C", "FiO2_1", "Potassium", 
+                      "Sodium", "Chloride", "Glucose", "BUN", "Creatinine", "Magnesium", "Calcium",
+                      "Ionised_Ca", "CO2_mEqL", "SGOT", "SGPT", "Total_bili", "Albumin", "Hb",                                         
+                      "WBC_count", "Platelets_count", "PTT", "PT", "INR", "Arterial_pH", "paO2", "paCO2",
+                      "Arterial_BE", "Arterial_lactate", "HCO3", "Shock_Index", "PaO2_FiO2")
+
+# Propensity Score with glmnet.
+fit <- cv.glmnet(x=xvars,y=sepdat_iv_dose_b1$iv_dose, family="binomial",alpha=1)
+lambda.in <- fit$lambda.1se
+# Covariates of interest.
+coef(fit,s='lambda.1se',exact=TRUE)
 
 # Covariates of interest.
-xvars <-  as.matrix( sepdat_iv_dose_b1[, names( which( coef(summary(prop_fit))[,4] < 0.000001 ) )] )
-
+xvars_final <- xvars[, names(which(coef(fit,s='lambda.1se',exact=TRUE)[-1,1] != 0)) ]
+dat_prop <- data.frame(sepdat_iv_dose_b1$iv_dose, xvars_final)
+prop_fit <- predict(glmnet(x=xvars,y=sepdat_iv_dose_b1$iv_dose, family="binomial",alpha=1),
+                    s=lambda.in, type = "response", newx = xvars)
 
 # real data application
-npar<-12
+npar<-dim(xvars_final)[2]
 eta0<-matrix(0,2*npar,npar)
 for(i in 1:npar){
-  eta0[2*i-1,i]<-1
-  eta0[2*i,i]<--1
+  eta0[2*i-1,i]<- 1
+  eta0[2*i,i]<- -1
 }
 
-eta0_guess <- eta0[2,]
+eta0_guess <- eta0[9,]
+
+AIPWE_b(Y=sepdat_iv_dose_b1$died_in_hosp, A=sepdat_iv_dose_b1$iv_dose, X=xvars_final,prop_score = prop_fit,eta0_guess)
+AIPWE_c(Z=sepdat_iv_dose_b1$reward, A=sepdat_iv_dose_b1$iv_dose, X=xvars_final,prop_score = prop_fit,eta0_guess)
+
+IPWE_b(Y=sepdat_iv_dose_b1$died_in_hosp, A=sepdat_iv_dose_b1$iv_dose, X=xvars_final,prop_score = prop_fit,eta0_guess)
+IPWE_c(Z=sepdat_iv_dose_b1$reward, A=sepdat_iv_dose_b1$iv_dose, X=xvars_final,prop_score = prop_fit,eta0_guess)
+
 
 # QP-IRL application.
-AL_HIV <- QP_IRL( Y= recode_dh, Z=sepdat_iv_dose_b1$reward , A=sepdat_iv_dose_b1$iv_dose, 
-                  X = xvars , prop_score = prop_fit$fitted.values , k.num=50, eps = 0.00001, 
+AL_HIV <- QP_IRL( Y= sepdat_iv_dose_b1$died_in_hosp, Z=sepdat_iv_dose_b1$reward , A=sepdat_iv_dose_b1$iv_dose, 
+                  X = xvars_final , prop_score = prop_fit , k.num=50, eps = 0.00001, 
                   lambda=0, eta0 = eta0_guess )
 
 
